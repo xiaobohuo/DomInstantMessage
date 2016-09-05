3e8c70f180cee2c0ceafaff47bef92705bff37ac
do_mination <xiaobohuo@gmail.com>
2016-09-06 01:16:23 +0800

    同步 9-6

--- app/src/main/java/com/dom/ination/dominstantmessage/base/MyApplication.java
+++ app/src/main/java/com/dom/ination/dominstantmessage/base/MyApplication.java
@@ -1,15 +1,19 @@
 package com.dom.ination.dominstantmessage.base;
 
 import android.app.Application;
+import android.os.Environment;
 
+import com.dom.ination.domforandroid.common.context.GlobalContext;
 import com.dom.ination.domforandroid.common.setting.SettingUtility;
+import com.dom.ination.domforandroid.component.bitmaploader.BitmapLoader;
 
+import java.io.File;
 import java.util.Random;
 
 /**
  * Created by huoxiaobo on 16/9/1.
  */
-public class MyApplication extends Application{
+public class MyApplication extends GlobalContext{
 
     @Override
     public void onCreate() {
@@ -20,6 +24,29 @@ public class MyApplication extends Application{
 
         // 初始化一个颜色主题
         setupTheme();
+        // 打开Debug日志
+        setupCrash();
+        //初始化图片加载
+        BitmapLoader.newInstance(this,getImagePath());
+    }
+
+    public static String getImagePath() {
+        return GlobalContext.getInstance().getExternalFilesDir(Environment.DIRECTORY_PICTURES).getAbsolutePath() + File.separator;
+    }
+
+    public void setupCrash() {
+//        if (BuildConfig.LOG_DEBUG) {
+//            CrashHandler.setupCrashHandler(this);
+//        }
+//        // UMENG统计设置
+//        MobclickAgent.setDebugMode(Logger.DEBUG);
+//        MobclickAgent.setCatchUncaughtExceptions(false);
+//        MobclickAgent.openActivityDurationTrack(false);
+//        if (BuildConfig.LOG_DEBUG) {
+//            Logger.d("Device_info", UMengUtil.getDeviceInfo(this));
+//        }
+//        // BUGLY日志上报
+//        CrashReport.initCrashReport(this, BuildConfig.BUGLY_APP_ID, Logger.DEBUG);
     }
 
     private void setupTheme() {
